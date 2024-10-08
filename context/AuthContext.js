792b304ed601bb6db421e18905f0351400e6ba5d
josue cast <89426463+josue-create@users.noreply.github.com>
2024-10-07 19:44:42 -0600

    Add files via upload

---
+++ context/AuthContext.js
@@ -0,0 +1,14 @@
+import {createContext, useState} from 'react';
+
+const AuthContext = createContext();
+
+const initialAuth =  null;
+
+const AuthContext = ({children}) =>{
+
+	const [auth, setAuth] = usestate
+
+
+
+
+}
---
+++ index.jsx
@@ -0,0 +1,7 @@
+const element = React.createElement('h2', {}, 'Hola');
+const container = document.getElementById("pre");
+
+ReactDOM.render(
+ element,
+ container
+);
